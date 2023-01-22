00db19c6f592a2158a82da92f6aa4ccd91334449
onemore8762 <denis.churkin.afikei@mail.ru>
2023-01-22 11:04:28 +0300

    add prettier and eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-root',
+  selector: 'todolist-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todolist-angular';
+  title = 'todolist-angular'
 }
